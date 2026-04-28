15876d5350270c2c0c894584da31caeaec1e21c7
wise <114738222+Wise-394@users.noreply.github.com>
2026-04-28 10:36:36 +0800

    feat: add user query functions for retrieving users by ID and username

---
+++ server/src/models/usersQuery.js
@@ -0,0 +1,34 @@
+import { pool } from "./databaseConfig.js";
+
+export const getUserById = async (id) => {
+  try {
+    const { rows } = await pool.query(
+      `
+        SELECT * FROM users WHERE id = $1 LIMIT 1
+    `,
+      [id],
+    );
+    return rows[0];
+  } catch (err) {
+    console.error("unable to  get user by id", err);
+    throw err;
+  }
+};
+
+export const getUserByUsername = async (username) => {
+  try {
+    const { rows } = await pool.query(
+      `
+        SELECT * FROM users WHERE username = $1
+        `,
+      [username],
+    );
+    return rows[0];
+  } catch (err) {
+    console.error("unable to get user by username", err);
+    throw err;
+  }
+};
+
+// export const insertUser = async(username, password, role);
+// TODO INSERT USER, PASSPORT JS LOCAL STRATEGY, REGISTER, LOGIN
