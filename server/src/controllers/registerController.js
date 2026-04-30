e3673d8dc0f770b954eca4e617abf86d59bb0b03
wise <114738222+Wise-394@users.noreply.github.com>
2026-04-30 22:20:30 +0800

    feat: add express-validator for user input validation and update registration logic

--- server/src/controllers/registerController.js
+++ server/src/controllers/registerController.js
@@ -1,8 +1,25 @@
-import { getUserByUsername } from "../models/usersQuery.js";
+import { getUserByUsername, insertUser } from "../models/usersQuery.js";
+import bcrypt from "bcrypt";
+
 export const checkIfUsernameExist = async (req, res, next) => {
   const users = await getUserByUsername(req.body.username);
   if (users) {
     return res.status(400).json({ message: "username already exist" });
   }
   next();
+};
+
+// TODO VALIDATE USER INPUT USING EXPRESS VALIDATOR
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const username = req.body.username;
+    const password = await bcrypt.hash(req.body.password, 10);
+
+    const id = await insertUser(username, password, "user");
+    return res.json({ id });
+  } catch (err) {
+    console.error("unable to register user", err);
+    next(new Error("unable to create user"));
+  }
 };
