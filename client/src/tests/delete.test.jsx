95e45c18d1a1103fac5bb21d0405f83b6b66050f
wise <114738222+Wise-394@users.noreply.github.com>
2026-05-27 16:22:25 +0800

    feat: add initial delete post tests for user authentication and error handling

---
+++ client/src/tests/delete.test.jsx
@@ -0,0 +1,9 @@
+import { desribe, it, expect, describe } from "vitest";
+
+describe("delete post", () => {
+  it("deletes user post");
+  it("redirect if not authenticated");
+  it("redirect if no id");
+  it("shows error when backend isnt online");
+  it("shows error when cannot delete post");
+});
